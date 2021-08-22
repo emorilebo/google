af7fe73d8823671bc00e8a220d4753c0a7229663
Godfrey Lebo <emorylebo@gmail,com>
2021-08-22 22:22:57 +0100

    getServerSideProps implemented

---
+++ Response.js
@@ -0,0 +1,425 @@
+export default {
+  kind: "customsearch#search",
+  url: {
+    type: "application/json",
+    template:
+      "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json",
+  },
+  queries: {
+    request: [
+      {
+        title: "Google Custom Search - List",
+        totalResults: "623000000",
+        searchTerms: "List",
+        count: 10,
+        startIndex: 1,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "efdb87e0c1938f752",
+      },
+    ],
+    nextPage: [
+      {
+        title: "Google Custom Search - List",
+        totalResults: "623000000",
+        searchTerms: "List",
+        count: 10,
+        startIndex: 11,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "efdb87e0c1938f752",
+      },
+    ],
+  },
+  context: {
+    title: "Google",
+  },
+  searchInformation: {
+    searchTime: 0.463971,
+    formattedSearchTime: "0.46",
+    totalResults: "623000000",
+    formattedTotalResults: "623,000,000",
+  },
+  items: [
+    {
+      kind: "customsearch#result",
+      title: "Lists - Material Design",
+      htmlTitle: "<b>Lists</b> - Material Design",
+      link: "https://www.google.com/design/spec/components/lists.html",
+      displayLink: "www.google.com",
+      snippet:
+        "A list control can display information and actions for list items. Lists with controls contain three content types: 1. Supporting visuals 2. Primary text 3 ...",
+      htmlSnippet:
+        "A <b>list</b> control can display information and actions for <b>list</b> items. <b>Lists</b> with controls contain three content types: 1. Supporting visuals 2. Primary text 3&nbsp;...",
+      cacheId: "gpfYxtyn6FsJ",
+      formattedUrl: "https://www.google.com/design/spec/components/lists.html",
+      htmlFormattedUrl:
+        "https://www.google.com/design/spec/components/<b>lists</b>.html",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQcjC_bTkXLWoGYcAg8tFBEmmnnkWF_9zU7ky_Lk3DfrQNDrsxwt7memy6a",
+            width: "318",
+            height: "159",
+          },
+        ],
+        metatags: [
+          {
+            "og:image":
+              "https://lh3.googleusercontent.com/60dO-exXa99q4CKQ7dgpoGeVYAVurzleVoEpjaE1zeBz3LYL-AidNsKgvaYWLOn1GrFsJ6zaShwdakKB7iKVV7EYyyk1Y6w2-YkI",
+            "twitter:title": "Material Design",
+            "og:type": "article",
+            "twitter:card": "summary_large_image",
+            "theme-color": "#ffffff",
+            "og:site_name": "Material Design",
+            "twitter:url": "https://material.io/components/lists",
+            "og:title": "Material Design",
+            "og:description":
+              "Lists are continuous, vertical display of text or images.",
+            "twitter:image": "https://material.io/static/assets/result.png",
+            viewport: "initial-scale=1, width=device-width",
+            "twitter:description":
+              "Build beautiful, usable products faster. Material Design is an adaptable system—backed by open-source code—that helps teams build high quality digital experiences.",
+            "format-detection": "telephone=no",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://lh3.googleusercontent.com/60dO-exXa99q4CKQ7dgpoGeVYAVurzleVoEpjaE1zeBz3LYL-AidNsKgvaYWLOn1GrFsJ6zaShwdakKB7iKVV7EYyyk1Y6w2-YkI",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Google Keep: Free Note Taking App for Personal Use",
+      htmlTitle: "Google Keep: Free Note Taking App for Personal Use",
+      link: "https://www.google.com/keep/",
+      displayLink: "www.google.com",
+      snippet:
+        "Add notes, lists, photos, and audio to Keep. ... Set a location-based reminder to pull up your grocery list right when you get to the store.",
+      htmlSnippet:
+        "Add notes, <b>lists</b>, photos, and audio to Keep. ... Set a location-based reminder to pull up your grocery <b>list</b> right when you get to the store.",
+      cacheId: "yt6B_ILL5GoJ",
+      formattedUrl: "https://www.google.com/keep/",
+      htmlFormattedUrl: "https://www.google.com/keep/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcROKnZbFgp4uP1tPkxn8NhZD0ETpg2polSzZwicyS_0H4gtAhDRhwz4c5s",
+            width: "225",
+            height: "225",
+          },
+        ],
+        metatags: [
+          {
+            "og:image": "//www.google.com/images/icons/product/keep-512.png",
+            viewport: "initial-scale=1, minimum-scale=1, width=device-width",
+            "og:title":
+              "Meet Google Keep – Save your thoughts, wherever you are",
+            "og:url": "https://www.google.com/keep/",
+            "og:description":
+              "Capture notes, share them with others, and access them from your computer, phone or tablet. Free with a Google account.",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://www.google.com/images/icons/product/keep-512.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "YouTube Channel Switcher",
+      htmlTitle: "YouTube Channel Switcher",
+      link: "https://www.google.com/appserve/mkt/p/AMJ1mutcrEVXkuA0VAKIRa1xnIpmF1ynrasp-kT2mWuUXg9DJ0XJQdzIf6b26gROndJY7jQfpfRy37IIQetLqYaC5VQ-N_A2xWWUY8QWHg",
+      displayLink: "www.google.com",
+      snippet: "Share your videos with friends, family, and the world.",
+      htmlSnippet: "Share your videos with friends, family, and the world.",
+      formattedUrl:
+        "https://www.google.com/.../AMJ1mutcrEVXkuA0VAKIRa1xnIpmF1ynrasp- kT2mWuUXg9DJ0XJQdzIf6b26gROndJY7jQfpfRy37IIQetLqYaC5VQ-N...",
+      htmlFormattedUrl:
+        "https://www.google.com/.../AMJ1mutcrEVXkuA0VAKIRa1xnIpmF1ynrasp- kT2mWuUXg9DJ0XJQdzIf6b26gROndJY7jQfpfRy37IIQetLqYaC5VQ-N...",
+      pagemap: {
+        metatags: [
+          {
+            viewport: "width=device-width, initial-scale=1",
+            "format-detection": "telephone=no",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Browse a list of Google's Office Locations - Google",
+      htmlTitle:
+        "Browse a <b>list</b> of Google&#39;s Office Locations - Google",
+      link: "https://www.google.com/about/locations/",
+      displayLink: "www.google.com",
+      snippet:
+        "Google has more than 70 offices in 50 countries. View a directory of our locations around the world.",
+      htmlSnippet:
+        "Google has more than 70 offices in 50 countries. View a directory of our locations around the world.",
+      cacheId: "jULhJEj16iYJ",
+      formattedUrl: "https://www.google.com/about/locations/",
+      htmlFormattedUrl: "https://www.google.com/about/locations/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTlZkPejiaVNk_2IyUGePqZLcAdf32oB7gk3s6XpECtF-ogmvHdfg36F9I3",
+            width: "310",
+            height: "163",
+          },
+        ],
+        metatags: [
+          {
+            "og:image":
+              "https://lh3.googleusercontent.com/gi7X34TTW6Uy2F1aiwO9N5GHmkftlVOmfdvWRKUrK3ASh4LV3cKMn-cIJKMj0AiLwVAqLqhouTxohCpQjxkrkk7T1op-503rC4DK6A=w600-l80-sg-rj",
+            "og:image:width": "600",
+            "og:type": "website",
+            "twitter:card": "summary_large_image",
+            "twitter:title":
+              "Browse a list of Google's Office Locations - Google",
+            "og:title": "Browse a list of Google's Office Locations - Google",
+            "og:image:height": "315",
+            "og:description":
+              "Google has more than 70 offices in 50 countries. View a directory of our locations around the world.",
+            "twitter:image":
+              "https://lh3.googleusercontent.com/gi7X34TTW6Uy2F1aiwO9N5GHmkftlVOmfdvWRKUrK3ASh4LV3cKMn-cIJKMj0AiLwVAqLqhouTxohCpQjxkrkk7T1op-503rC4DK6A=w600-l80-sg-rj",
+            "google:search-sre-monitor": "ALL_ALL",
+            "twitter:site": "@google",
+            viewport: "initial-scale=1, minimum-scale=1, width=device-width",
+            "og:url": "//www.google.com/locations/",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://lh3.googleusercontent.com/gi7X34TTW6Uy2F1aiwO9N5GHmkftlVOmfdvWRKUrK3ASh4LV3cKMn-cIJKMj0AiLwVAqLqhouTxohCpQjxkrkk7T1op-503rC4DK6A=w600-l80-sg-rj",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "The Foodie List: Muncie - Google Maps",
+      htmlTitle: "The Foodie <b>List</b>: Muncie - Google Maps",
+      link: "https://www.google.com/maps/@40.2024741,-85.4342295,13z/data=!3m1!4b1!4m2!11m1!2s1C1pJUtR0v8On64jDQU2_FWcpcBs",
+      displayLink: "www.google.com",
+      snippet:
+        "The Foodie List: Muncie. By Google Lists · 20 places. Follow. Where food lovers are eating right now. Based on visits & the latest ratings. Updated weekly.",
+      htmlSnippet:
+        "The Foodie <b>List</b>: Muncie. By Google <b>Lists</b> · 20 places. Follow. Where food lovers are eating right now. Based on visits &amp; the latest ratings. Updated weekly.",
+      cacheId: "S8Ndh9o84uIJ",
+      formattedUrl:
+        "https://www.google.com/.../data= !3m1!4b1!4m2!11m1!2s1C1pJUtR0v8On64jDQU2_FWcpcBs",
+      htmlFormattedUrl:
+        "https://www.google.com/.../data= !3m1!4b1!4m2!11m1!2s1C1pJUtR0v8On64jDQU2_FWcpcBs",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_lWkIXB7fz4tuNLq6APyycduxPbLy8aT9na9rVV0QebsRY9U8aKLvLURk",
+            width: "225",
+            height: "225",
+          },
+        ],
+        metatags: [
+          {
+            referrer: "origin",
+            "og:image":
+              "https://lh5.googleusercontent.com/p/AF1QipP7f0SIZvx6juOmnq97GPpam-AmZ-FlYK1I9nn5=w256-h256-k-no-p",
+            "og:image:width": "256",
+            "twitter:card": "summary",
+            "og:site_name": 'Google Lists\'s "The Foodie List: Muncie" List',
+            viewport:
+              "initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no",
+            "og:title": 'Google Lists\'s "The Foodie List: Muncie" List',
+            "og:image:height": "256",
+            google: "notranslate",
+            "og:description":
+              "20 places - Where food lovers are eating right now. Based on visits & the latest ratings. Updated weekly.",
+          },
+        ],
+        place: [
+          {
+            image:
+              "https://lh5.googleusercontent.com/p/AF1QipP7f0SIZvx6juOmnq97GPpam-AmZ-FlYK1I9nn5=w256-h256-k-no-p",
+            name: 'Google Lists\'s "The Foodie List: Muncie" List',
+            description:
+              "20 places - Where food lovers are eating right now. Based on visits & the latest ratings. Updated weekly.",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://lh5.googleusercontent.com/p/AF1QipP7f0SIZvx6juOmnq97GPpam-AmZ-FlYK1I9nn5=w256-h256-k-no-p",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Brand Resource Center | Trademark list",
+      htmlTitle: "Brand Resource Center | Trademark <b>list</b>",
+      link: "https://www.google.com/permissions/trademark/trademark-list/",
+      displayLink: "www.google.com",
+      snippet:
+        "Illustrative, non-exhaustive list of trademarks owned by Google LLC · ADLINGO™ marketing platform · Alooma ™ cloud software · Anthos™ multi-cloud application ...",
+      htmlSnippet:
+        "Illustrative, non-exhaustive <b>list</b> of trademarks owned by Google LLC &middot; ADLINGO™ marketing platform &middot; Alooma ™ cloud software &middot; Anthos™ multi-cloud application&nbsp;...",
+      cacheId: "-0UoPwueLqEJ",
+      formattedUrl:
+        "https://www.google.com/permissions/trademark/trademark-list/",
+      htmlFormattedUrl:
+        "https://www.google.com/permissions/trademark/trademark-<b>list</b>/",
+      pagemap: {
+        metatags: [
+          {
+            referrer: "no-referrer",
+            viewport: "initial-scale=1, minimum-scale=1, width=device-width",
+            "og:url":
+              "https://about.google/brand-resource-center/trademark-list/",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Cloud-ready printers – Google Cloud Print",
+      htmlTitle: "Cloud-ready printers – Google Cloud Print",
+      link: "https://www.google.com/cloudprint/learn/printers/",
+      displayLink: "www.google.com",
+      snippet:
+        "Note: After December 31, 2020, Google Cloud Print will no longer be supported. Learn more about your migration options. Cloud-ready printers.",
+      htmlSnippet:
+        "Note: After December 31, 2020, Google Cloud Print will no longer be supported. Learn more about your migration options. Cloud-ready printers.",
+      cacheId: "Bq6S9Zy1hUYJ",
+      formattedUrl: "https://www.google.com/cloudprint/learn/printers/",
+      htmlFormattedUrl: "https://www.google.com/cloudprint/learn/printers/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSO0A6QfNNGY3BVJChYa5QI21sZqx7e5j1ErPmsz8btLrSbps52-bTFyI",
+            width: "247",
+            height: "204",
+          },
+        ],
+        metatags: [
+          {
+            "og:type": "website",
+            "og:site_name": "Google Cloud Print",
+            viewport:
+              "width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0",
+            "og:title": "Google Cloud Print",
+            "og:url": "https://www.google.com/cloudprint/learn/printers/",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://www.google.com/cloudprint/learn/static/images/logo_cloud_print.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Transparency – U.S. Public Policy – Google",
+      htmlTitle: "Transparency – U.S. Public Policy – Google",
+      link: "https://www.google.com/publicpolicy/transparency/",
+      displayLink: "www.google.com",
+      snippet:
+        "At Google, we believe it is important to have a voice in the political process to ensure the Internet remains a powerful engine for growth and innovation.",
+      htmlSnippet:
+        "At Google, we believe it is important to have a voice in the political process to ensure the Internet remains a powerful engine for growth and innovation.",
+      cacheId: "hcVZVdwcBcUJ",
+      formattedUrl: "https://www.google.com/publicpolicy/transparency/",
+      htmlFormattedUrl: "https://www.google.com/publicpolicy/transparency/",
+      pagemap: {
+        metatags: [
+          {
+            viewport: "initial-scale=1, minimum-scale=1, width=device-width",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Logos list - Brand Resource Center",
+      htmlTitle: "Logos <b>list</b> - Brand Resource Center",
+      link: "https://www.google.com/permissions/trademark/logos-list/",
+      displayLink: "www.google.com",
+      snippet:
+        "Logos list. Illustrative, non-exhaustive list of trademarks owned by Google LLC. Admin. Admin. AdMob. AdMob. Ads. Ads. AdSense. AdSense. AIY. AIY. Android.",
+      htmlSnippet:
+        "Logos <b>list</b>. Illustrative, non-exhaustive <b>list</b> of trademarks owned by Google LLC. Admin. Admin. AdMob. AdMob. Ads. Ads. AdSense. AdSense. AIY. AIY. Android.",
+      cacheId: "IGW6X9u1288J",
+      formattedUrl: "https://www.google.com/permissions/trademark/logos-list/",
+      htmlFormattedUrl:
+        "https://www.google.com/permissions/trademark/logos-<b>list</b>/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSor345qCIkoLkLW5jFCK4xn1LiGhNVzHAYlFMU2bePy_05EkgbSQ6ijNkD",
+            width: "225",
+            height: "225",
+          },
+        ],
+        metatags: [
+          {
+            referrer: "no-referrer",
+            viewport: "initial-scale=1, minimum-scale=1, width=device-width",
+            "og:url": "https://about.google/brand-resource-center/logos-list/",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://lh3.googleusercontent.com/wAPeTvxh_EwOisF8kMR2L2eOrIOzjfA5AjE28W5asyfGeH85glwrO6zyqL71dCC26R63chADTO7DLOjnqRoXXOAB8t2f4C3QnU6o0BA",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Google Ads Certifications : Google",
+      htmlTitle: "Google Ads Certifications : Google",
+      link: "https://www.google.com/appserve/mkt/p/AFnwnKVxW8DwCu98P8C27bSBPuAfmZVBAfLr17gOD_nt8FH2J0-de-X1bg2RPdu-JRhZjolEi2kNTDr6mJBcDJ00YoMECzureNzY8LEZnBOjdn8o3JGVtGUyL2eR-i9P-T8yMdk2VvV3_Wnp0q1ZtjnxIHjCrSgL12jgY7VYgExvy_a6s9gqHuIht2qRMEN-rtXsWxhIpzAqTJyKhkdULmqkKMOp4w5DQtpD20HzRXUcqz80_ICKVQ",
+      displayLink: "www.google.com",
+      snippet:
+        "Notice: Checking or unchecking a checkbox in this filter set will change the results list immediately. Duration. Short (0-5 min); Medium (6-20 min)",
+      htmlSnippet:
+        "Notice: Checking or unchecking a checkbox in this filter set will change the results <b>list</b> immediately. Duration. Short (0-5 min); Medium (6-20 min)",
+      cacheId: "Ng-dtNfR53oJ",
+      formattedUrl:
+        "https://www.google.com/.../ AFnwnKVxW8DwCu98P8C27bSBPuAfmZVBAfLr17gOD_nt8FH2J0-de- ...",
+      htmlFormattedUrl:
+        "https://www.google.com/.../ AFnwnKVxW8DwCu98P8C27bSBPuAfmZVBAfLr17gOD_nt8FH2J0-de- ...",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRUgbEDw1PrSoizuCiJ5nIycY7Lu1Vig7ECfvcosCfDz75EjMLJBz1Bi4Q",
+            width: "300",
+            height: "168",
+          },
+        ],
+        metatags: [
+          {
+            "og:image":
+              "https://storage.googleapis.com/exceedlms-external-uploads-production/uploads/categories/pictures/2844/original/unnamed.png",
+            "apple-mobile-web-app-status-bar-style": "white",
+            viewport: "width=device-width, initial-scale=1.0",
+            "apple-mobile-web-app-capable": "yes",
+            "csrf-token":
+              "+c6f7aSMEB59xD44kewk1zs3XwQbr5VXYPyfjRSB9q574Nd3xEV6ZfRlSZBN1ehw2jBd6/oM5/rNhekt9J3lyQ==",
+            "csrf-param": "authenticity_token",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://storage.googleapis.com/exceedlms-external-uploads-production/uploads/categories/pictures/2844/original/unnamed.png",
+          },
+        ],
+      },
+    },
+  ],
+};
--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import { API_KEY, CONTEXT_KEY } from "../keys";
+import Response from "../Response";
 
-function Search() {
+function Search({ results }) {
+  console.log(results);
   return (
     <div>
       <Head>
@@ -16,3 +19,20 @@ function Search() {
 }
 
 export default Search;
+
+export async function getServerSideProps(context) {
+  const useDummyData = true;
+  const startIndex = context.query.start || "0";
+  const data = useDummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+      ).then((response) => response.json());
+
+  //After the SERVER has rended........Pass the result to the client side
+  return {
+    props: {
+      results: data,
+    },
+  };
+}
