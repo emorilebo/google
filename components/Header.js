bd77dfb94a0cae3b2d774d11034993147cdbf1e3
Godfrey Lebo <emorylebo@gmail,com>
2021-08-22 17:53:02 +0100

    header section of search

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,19 @@
-import { useRouter } from "next/dist/client/router";
+import { useRouter } from "next/router";
 import Image from "next/image";
+import { useRef } from "react";
+import { SearchIcon, XIcon } from "@heroicons/react/solid";
+import { MicrophoneIcon } from "@heroicons/react/outline";
 
 function Header() {
   const router = useRouter();
+  const searchInputRef = useRef(null);
+  const search = (e) => {
+    e.preventDefault();
+
+    const term = searchInputRef.current.value;
+    if (!term) return;
+    router.push(`/search/${term}`);
+  };
   return (
     <header>
       <Image
@@ -12,6 +23,22 @@ function Header() {
         onClick={() => router.push("/")}
         className="cursor-pointer"
       />
+      <form className="flex px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
+        <input
+          ref={searchInputRef}
+          className="flex-grow w-full focus:outline-none"
+          type="text"
+        />
+        <XIcon
+          className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
+          onClick={() => (searchInputRef.current.value = "")}
+        />
+        <MicrophoneIcon className="mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300" />
+        <SearchIcon className="h-6 text-blue-500 hidden sm:inline-flex" />
+        <button hidden type="submit" onClick={search}>
+          Search
+        </button>
+      </form>
     </header>
   );
 }
